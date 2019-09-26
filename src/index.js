9c2303f53b138fc4e99428d54435a287e3cb6242
Robsondj <robson.djsilva@gmail.com>
2019-09-26 17:25:47 -0300

    Alterado react-router para o react-router-dom que é uma versão mais recente utilizada somente para web, já que a partir da versão 4 do react-router ele foi desmembrado em uma versão para o react-native e uma web para react

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import AutorBox from './Autor';
 import Home from './Home';
-import {Router,Route,browserHistory,IndexRoute} from 'react-router';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	(<Router history={browserHistory}>
-		<Route path="/" component={App}>
-		  <IndexRoute component={Home}/>
-		  <Route path="/autor" component={AutorBox}/>
-		  <Route path="/livro"/>
-		</Route>
+	(<Router>
+		<App>
+		  <Switch>
+			<Route exact path="/" component={Home}/>
+			<Route path="/autor" component={AutorBox}/>
+			<Route path="/livro"/>
+		  </Switch>
+		</App>
     </Router>), 
     document.getElementById('root'));
 
