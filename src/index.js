a75f20236f402520dcc55a7c87609edc8f570e6e
Robsondj <robson.djsilva@gmail.com>
2019-09-26 17:13:31 -0300

    Adicionando rotas para a aplicação sem fazer reload da página com o react-router 3.2.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
+import {Router,Route,browserHistory,IndexRoute} from 'react-router';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	(<Router history={browserHistory}>
+		<Route path="/" component={App}>
+		  <IndexRoute component={Home}/>
+		  <Route path="/autor" component={AutorBox}/>
+		  <Route path="/livro"/>
+		</Route>
+    </Router>), 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
